87a1e8a37c70126f4419da0067cc3b6d4abf8453
Design <design@sedc.mk>
2018-01-25 20:57:25 +0100

    Introduction, variables, aritmetic operations

---
+++ cas01/js/script.js
@@ -0,0 +1,75 @@
+// alert("Hello class");
+console.log(2+3/5); //logs in console
+
+//dsfsdf
+//sdfsdf
+//sdfsdf
+/*
+
+multi line
+
+comment
+
+*/
+// document.write ('text/html to be written to the page');
+
+let firstName; //declaration of variable firstName
+
+console.log(firstName); //undefined
+
+firstName = "SEDC"; //assigning value
+
+console.log(firstName); //SEDC
+
+let lastName = "Design"; //declare and assign
+
+lastName = "Design Academy";
+
+console.log(lastName); //Design Academy
+
+let age = 50;
+
+console.log(typeof age);
+
+let single = false;
+
+//Block code
+if (true) {
+	var b = 10;
+	let a = 12; //block level variable
+	console.log(a); //ok
+}
+
+//console.log(a); //not ok, a does not exist
+console.log(b); //10
+
+const DEBUG = true;
+
+let C = 1;
+c = 2; 
+
+((1 + 4) / 6 ) * 2;
+
+let res = 1 + 2;
+console.log( ((1 + 4) / 6 ) * 2 );
+
+console.log( 2 % 10 ); //reminder from devision
+
+//res = 3
+res = res + 1;  //add one to res
+//res = 4
+
+//equvalent
+res += 1;  //add one to res
+res++; //add one to res
+
+let x = 3;
+let y = 4;
+let P = x * y;
+
+console.log("The area is " + P + ". Hello!");
+
+document.write("<p data-id='" + x + "'>New p</p>");
+document.write(`<p data-id="${x}">Second new</p>`);
+
+console.log("He said: \"Are you coming?\" ");
